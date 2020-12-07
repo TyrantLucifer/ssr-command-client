9a207f2ece350f2981b1130864ebb0b9e04e51cf
Thatcher.Tian <thatcher.tian@homecreditcfc.cn>
2020-12-07 09:54:19 +0800

    修复windows获取家目录的bug，减少导入不必要的模块

--- test.py
+++ test.py
@@ -11,21 +11,21 @@ from utils.SettingUtils import Setting
 from utils.SsrTestUtils import SsrSpeedTest
 
 i = InitConfigDir()
-g = GetSubscribeUrl()
-settings = Setting(i.configFilePath)
-s = SsrSpeedTest()
-
-subscribeUrlList = settings.valueDict['SUBSCRIBE_URL'].split(',')
-resultList = g.requestUrlList(subscribeUrlList)
-ssrList = g.getNodeInfoList()
-pool = Pool(len(ssrList))
-thread = list()
-for ssr in ssrList:
-    t = pool.apply_async(s.testSsrNodes, (ssr,))
-    thread.append(t)
-
-pool.close()
-pool.join()
-
-for t in thread:
-    print(t.get()['ping'])
+# g = GetSubscribeUrl()
+# settings = Setting(i.configFilePath)
+# s = SsrSpeedTest()
+#
+# subscribeUrlList = settings.valueDict['SUBSCRIBE_URL'].split(',')
+# resultList = g.requestUrlList(subscribeUrlList)
+# ssrList = g.getNodeInfoList()
+# pool = Pool(len(ssrList))
+# thread = list()
+# for ssr in ssrList:
+#     t = pool.apply_async(s.testSsrNodes, (ssr,))
+#     thread.append(t)
+#
+# pool.close()
+# pool.join()
+#
+# for t in thread:
+#     print(t.get()['ping'])

9233eab0e5ee49375cb5838a95c5fa46880f7b1a
TyrantLucifer <TyrantLucifer@gmail.com>
2021-03-13 14:27:22 +0800

    删除多余逗号

--- shadowsocksr_cli/functions.py
+++ shadowsocksr_cli/functions.py
@@ -94,7 +94,7 @@ class DisplayShadowsocksr(object):
             logger.info("Current version {0} is newest. Please enjoy.".format(version))
         else:
             logger.info("Current version: {0}".format(init_config.version))
-            logger.info("Newest version: {0}, ".format(version))
+            logger.info("Newest version: {0}".format(version))
             logger.info("You can use 'pip(pip3) install shadowsocksr-cli' to update.")
         logger.info(tips_message)
 

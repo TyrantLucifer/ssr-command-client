c315af2d5598a120a71c43a2899231a87f8833b8
tyrantlucifer <tyrantlucifer@gmail.com>
2022-04-04 19:50:35 +0800

    update version to 2.1.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with codecs.open('README.rst', encoding='utf-8') as f:
 
 setup(
     name="shadowsocksr-cli",
-    version="2.1.7",
+    version="2.1.8",
     author="tyrantlucifer",
     author_email="tyrantlucifer@gmail.com",
     description="The command client of shadowsocksr",

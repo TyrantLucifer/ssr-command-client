d4028acdf479f4027fe96d1a16c1343584c4a07d
tyrantlucifer <tyrantlucifer@gmail.com>
2022-04-05 19:14:11 +0800

    update version to 2.1.9

--- shadowsocksr_cli/init_utils.py
+++ shadowsocksr_cli/init_utils.py
@@ -40,7 +40,7 @@ class InitConfig(object):
     """
 
     def __init__(self):
-        self.version = '2.1.8'
+        self.version = '2.1.9'
         self.platform = sys.platform
         self.system = platform.uname().version
         self.home_dir = os.path.expanduser('~')
